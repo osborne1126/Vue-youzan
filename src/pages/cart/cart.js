dae8bc7516839ec3f30630d21a916f334737324b
osborne1126 <1126340628@qq.com>
2018-02-11 20:40:30 +0800

    购物车-数据渲染 商品选择的处理

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,3 +1,67 @@
 import './cart_base.css'
 import './cart_trade.css'
 import './cart.css'
+
+import Vue from 'vue'
+import mixin from 'js/mixin.js'
+import axios from 'axios'
+import url from 'js/api.js'
+
+new Vue({
+    el: '.container',
+    data: {
+        lists: null
+    },
+    computed: {
+
+    },
+    created() {
+        this.getList()
+    },
+    methods: {
+        getList() {
+            axios.post(url.cartLists).then(res => {
+                let lists = res.data.cartList
+                lists.forEach(shop => {
+                    shop.checked = true
+                    shop.goodsList.forEach(good => {
+                        good.checked = true
+                    })
+                })
+                this.lists = lists
+            })
+        },
+        selectGood(good) {
+            //console.log(good.checked)
+            good.checked = !good.checked
+        }
+    },
+    mixins: [mixin]
+})
+
+
+
+
+
+
+
+
+
+
+
+/*
+//通过mock模拟数据写法 （需先安装mockjs）
+import Mock from 'mockjs'
+let Random = Mock.Random
+
+let data = Mock.mock({
+    'cartList|3' :[{
+        'goodsList|1-2' :[{
+            id: Random.int(10000,100000),
+            image: Mock.mock('@img(90x90,@color)')
+        }]
+    }]
+})
+
+console.log(data)
+ */
