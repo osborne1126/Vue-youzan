4e993e1a8930ff5db77563598905d02d69a13075
osborne1126 <1126340628@qq.com>
2018-02-13 18:50:19 +0800

    购物车-编辑状态：增、减

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -13,7 +13,9 @@ new Vue({
         lists: null,
         total: 0,
         editingShop: null,
-        editingShopIndex: -1
+        editingShopIndex: -1,
+        removePopup: false,
+        removeData: null
     },
     computed: {
         allSelected: {
@@ -34,6 +36,22 @@ new Vue({
                 })
             }
         },
+        allRemoveSelected: {
+            get() {
+                if(this.editingShop) {
+                    return this.editingShop.removeChecked
+                }
+                return false
+            },
+            set(newVal) {
+                if(this.editingShop) {
+                    this.editingShop.removeChecked = newVal
+                    this.editingShop.goodsLists.forEach(good => {
+                        good.removeChecked = newVal
+                    })
+                }
+            }
+        },
         selectLists() {
             if(this.lists&&this.lists.length){
                 let arr = []
@@ -52,7 +70,16 @@ new Vue({
             return []
         },
         removeLists() {
-            //
+            if(this.editingShop) {
+                let arr = []
+                this.editingShop.goodsList.forEach(good => {
+                    if(good.removeChecked) {
+                        arr.push(good)
+                    }
+                })
+                return arr
+            }
+            return []
         }
     },
     created() {
@@ -76,21 +103,22 @@ new Vue({
             })
         },
         selectGood(shop,good) {
-            //console.log(good.checked)
-            good.checked = !good.checked
-            shop.checked = shop.goodsList.every(good => {
-                return good.checked
+            let attr = this.editingShop ? 'removeChecked' : 'checked'
+            good[attr] = !good[attr]
+            shop[attr] = shop.goodsList.every(good => {
+                return good[attr]
             })
       },
       selectShop(shop) {
-          shop.checked = !shop.checked
+        let attr = this.editingShop ? 'removeChecked' : 'checked'
+          shop[attr] = !shop[attr]
           shop.goodsList.forEach(good => {
-              good.checked = shop.checked
+              good[attr] = shop[attr]
           })
       },
       selectAll() {
-          //console.log(this.allSelected)
-          this.allSelected = !this.allSelected
+        let attr = this.editingShop ? 'allRemoveSelected' : 'allSelected'
+          this[attr] = !this[attr]
       },
       edit(shop,shopIndex) {
           //console.log(shop.editingMsg)
@@ -105,6 +133,49 @@ new Vue({
           })
           this.editingShop = shop.editing ? shop : null
           this.editingShopIndex = shop.editing ? shopIndex : -1
+      },
+      reduce(good) {
+          if(good.number===1) return
+          axios.post(url.cartReduce, {
+              id: good.id,
+              number: 1
+          }).then(res => {
+              good.number--
+          })
+      },
+      add(good) {
+          axios.post(url.cartAdd, {
+              id: good.id,
+              number: 1
+          }).then(res => {
+              good.number++
+          })
+      },
+      remove(shop,shopIndex,good,goodIndex) {
+          //console.log('remove')
+          this.removePopup = true
+          this.removeData = {shop,shopIndex,good,goodIndex}
+      },
+      removeConfirm() {
+          let {shop,shopIndex,good,goodIndex} = this.removeData
+          axios.post(url.cartRemove,{
+              id: good.id
+          }).then(res => {
+              shop.goodsList.splice(goodIndex, 1)
+              if(!shop.goodsList.length) {
+                  this.lists.splice(shopIndex, 1)
+                  this.removeShop()
+              }
+              this.removePopup = false
+          })
+      },
+      removeShop() {
+          this.editingShop = null
+          this.editingShopIndex = -1
+          this.lists.forEach(shop => {
+              shop.editing = false
+              shop.editingMsg = '编辑'
+          })
       }
     },
     mixins: [mixin]
