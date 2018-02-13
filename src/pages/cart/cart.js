00c810988b94411edb5f267890ecd5be5a387f4e
osborne1126 <1126340628@qq.com>
2018-02-13 12:38:46 +0800

    购物车-正常状态和编辑状态的切换

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -10,10 +10,50 @@ import url from 'js/api.js'
 new Vue({
     el: '.container',
     data: {
-        lists: null
+        lists: null,
+        total: 0,
+        editingShop: null,
+        editingShopIndex: -1
     },
     computed: {
-
+        allSelected: {
+            get() {
+                if(this.lists&&this.lists.length){
+                    return this.lists.every(shop => {
+                        return shop.checked
+                    })
+                }
+                return false
+            },
+            set(newVal) {
+                this.lists.forEach(shop => {
+                    shop.checked = newVal
+                    shop.goodsList.forEach(good => {
+                        good.checked = newVal
+                    })
+                })
+            }
+        },
+        selectLists() {
+            if(this.lists&&this.lists.length){
+                let arr = []
+                let total = 0
+                this.lists.forEach(shop => {
+                    shop.goodsList.forEach(good => {
+                        if(good.checked) {
+                            arr.push(good)
+                            total += good.price * good.number
+                        }
+                    })
+                })
+                this.total = total
+                return arr
+            }
+            return []
+        },
+        removeLists() {
+            //
+        }
     },
     created() {
         this.getList()
@@ -24,17 +64,48 @@ new Vue({
                 let lists = res.data.cartList
                 lists.forEach(shop => {
                     shop.checked = true
+                    shop.removeChecked = false
+                    shop.editing = false
+                    shop.editingMsg = '编辑'
                     shop.goodsList.forEach(good => {
                         good.checked = true
+                        good.removeChecked = false
                     })
                 })
                 this.lists = lists
             })
         },
-        selectGood(good) {
+        selectGood(shop,good) {
             //console.log(good.checked)
             good.checked = !good.checked
-        }
+            shop.checked = shop.goodsList.every(good => {
+                return good.checked
+            })
+      },
+      selectShop(shop) {
+          shop.checked = !shop.checked
+          shop.goodsList.forEach(good => {
+              good.checked = shop.checked
+          })
+      },
+      selectAll() {
+          //console.log(this.allSelected)
+          this.allSelected = !this.allSelected
+      },
+      edit(shop,shopIndex) {
+          //console.log(shop.editingMsg)
+          shop.editing = !shop.editing
+          //console.log(shop.editing)
+          shop.editingMsg = shop.editing ? '完成' : '编辑'
+          this.lists.forEach((item,i) => {
+              if(shopIndex !== i) {
+                  item.editing = false
+                  item.editingMsg = shop.editing ? '' : '编辑'
+              }
+          })
+          this.editingShop = shop.editing ? shop : null
+          this.editingShopIndex = shop.editing ? shopIndex : -1
+      }
     },
     mixins: [mixin]
 })
